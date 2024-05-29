0e6283669925a369c2fc285a0d5d52105be0a502
이민정 <mj.lee@mrblue.com>
2024-05-29 17:57:03 +0900

    feat: scss, stylex 초기 설정

---
+++ components/Test.tsx
@@ -0,0 +1,3 @@
+export default function Test () {
+	return <div>오예오예</div>;
+}
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+const stylexPlugin = require('@stylexjs/nextjs-plugin');
+
+module.exports = stylexPlugin({
+    aliases: {
+        '@/*': [path.join(__dirname, '*')],
+    },
+    rootDir: __dirname,
+})({});
